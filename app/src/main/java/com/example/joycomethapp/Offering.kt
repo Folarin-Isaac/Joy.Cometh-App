828eb9a43b353457512f8440bdc9632821b5d1ea
idemudiaotes <idemudiaprecious96@gmail.com>
2020-10-05 16:54:59 +0100

    my initial commit

---
+++ app/src/main/java/com/example/joycomethapp/Offering.kt
@@ -0,0 +1,11 @@
+package com.example.joycomethapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Offering : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_offering)
+    }
+}
